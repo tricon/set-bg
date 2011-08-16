09d4e20116f563d6ece1a17b3998371c8a74aa50
David Aaron Fendley <tricon@me.com>
2011-08-16 12:57:30 -0500

    Added base-hostname and updated install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@ echo "Installing..."
 # Apple.png gets installed here. Should probably find a better place to put it.
 sudo cp /tmp/set-bg/* /usr/bin/
 rm -rf /tmp/set-bg
+
+# Update .zshrc
 echo "alias ssh=sshb" >> ~/.zshrc
+echo '# Set our fancy background.
+if [ ! -z $PS1 ]; then
+  hostname=`hostname`
+  name=`/usr/bin/base-hostname $hostname`
+  name=`echo $name | tr "[A-Z]" "[a-z]"`
+  /usr/bin/set-bg-host "$name"
+fi' >> ~/.zshrc
 
 echo "Done!"
